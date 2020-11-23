5e78ce29252d57ed003b42a304244d6215f6c49c
Alex <alexdodd@gmail.com>
2020-11-23 15:47:50 +0000

    finished

--- single_class_lab_start_code/team_class/src/team.py
+++ single_class_lab_start_code/team_class/src/team.py
@@ -3,6 +3,14 @@ class Team:
         self.name = name
         self.players = players
         self.coach = coach
+        self.points = 0
 
     def add_player(self, name):
         self.players.append(name)
+
+    def has_player(self, name):
+        return name in self.players
+
+    def play_game(self, win):
+        if win == True:
+            self.points += 3
