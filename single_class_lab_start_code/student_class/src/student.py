297122a48a967f49243d2922e69751add3b875c7
Alex <alexdodd@gmail.com>
2020-11-23 14:29:37 +0000

    student classs completed

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -1,1 +1,12 @@
+class Student:
+    def __init__(self, name, cohort):
+        self.name = name
+        self.cohort = cohort
+        
+    def talk(self):
+        return "I can talk!"
+
+    def say_favourite_language(self, fav_lang):
+        return "I love " + fav_lang
+
 
